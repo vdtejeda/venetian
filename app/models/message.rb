ea7e83fb50e0d2cbe27e684401805689f492463b
Volney Tejeda <volneytejeda10@gmail.com>
2015-12-03 23:51:01 -0500

    add better stylig to forms

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,8 +9,8 @@ class Message
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :email, presence: true, length: {maximum: 255 },
-                                    format: {with: VALID_EMAIL_REGEX}
+  validates :email, length: {maximum: 255 },
+                    format: {with: VALID_EMAIL_REGEX}
 
-                                    
+
 end
